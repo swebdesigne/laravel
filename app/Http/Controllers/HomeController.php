960fc71574186574ddb6f62a3893ebe1e1ce4d04
Igor <swebdesigne@gmail.com>
2021-03-13 20:07:37 +0300

    Configuration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,15 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        dump(env('APP_KEY1'));
+        dump(config('database.connections.mysql.database'));
         return view('welcome', ['res' => 5, 'name' => 'john']);
     }
 
-    public function test() {
+    public function test()
+    {
         return __METHOD__;
     }
 }
