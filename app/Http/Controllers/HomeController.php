63d6f3c4b722fdc2c48359e7ef359bb17c400bb3
Igor <swebdesigne@gmail.com>
2021-03-14 19:18:36 +0300

    Work with database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,22 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
     public function index()
     {
-        dump(env('APP_KEY1'));
-        dump(config('database.connections.mysql.database'));
+//        DB::insert("INSERT INTO posts (title, content, created_at, updated_at)
+//            VALUES (?, ?, ?, ?)", ['article 4', 'lorem ipsum 4', NOW(), NOW()]);
+
+        DB::update("UPDATE posts SET title = :title, created_at = :created_at, updated_at = :updated_at WHERE title = ''", [':title' => "article 0", ':created_at' => NOW(), 'updated_at' => NOW()]);
+        $posts = DB::select("SELECT * FROM posts WHERE id > :id", ['id' => 2]);
+        dd($posts);
+        return $posts;
+//        dump(env('APP_KEY1'));
+//        dump(config('database.connections.mysql.database'));
+
         return view('welcome', ['res' => 5, 'name' => 'john']);
     }
 
