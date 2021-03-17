697189c7de8f103798b4e1cef89178e8038111ab
Igor <swebdesigne@gmail.com>
2021-03-17 22:14:57 +0300

    SQL request, transactions

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function show($slug) {
+        return view("pages.show", ['slug' => $slug]);
+    }
+}
